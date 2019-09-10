3cbb2e1ade69d2a2bf577ca63a33a1cd76c00207
Ryo <r1995ngspring@gmail.com>
2019-09-10 22:26:00 +0900

    TransitionManagerとManagerシーンの追加 Tileシーンでクリックで確認可能

---
+++ gittest/Assets/Tools/Transiton/Scripts/TransitionManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーン管理　シングルトン
+/// </summary>
+public class TransitionManager : SingletonMonoBehaviour<TransitionManager>
+{
+
+    private MaskTransition m_MaskTransition;
+
+    //初期化メソッド(初アクセスまたはAwake時のどちらか一度だけ実行される)
+    protected override void Init()
+    {
+        base.Init();
+
+        // ---------- オブジェクトが破棄されないように --------------
+        DontDestroyOnLoad(this.gameObject);
+
+        // ---------- コンポーネント取得 ----------
+        m_MaskTransition = GetComponent<MaskTransition>();
+    }
+
+
+    public void ChangeSceneTransiton(string sceneName,float time)
+    {
+        StartCoroutine(LoadScene(sceneName, time));
+    }
+
+    IEnumerator LoadScene(string sceneName, float time)
+    {
+        // ----- フェードイン -----
+        m_MaskTransition.BeginTransition_In(time);
+
+        // ----- フェードイン終了まで待つ ----
+        while (m_MaskTransition.Running)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        // ----- シーンのロード -----
+        SceneManager.LoadScene(sceneName);
+
+
+        // ----- フェーアウト -----
+        m_MaskTransition.BeginTransition_Out(time);
+
+        // ----- フェードイン終了まで待つ ----
+        while (m_MaskTransition.Running)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+
+        yield return null;
+    }
+
+}
