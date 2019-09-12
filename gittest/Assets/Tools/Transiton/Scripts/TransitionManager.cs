97940ee20acd6863791bcf808151466423f0e12d
Ryo <r1995ngspring@gmail.com>
2019-09-12 21:28:17 +0900

    プログレスバーの追加(ロード早すぎて、正しい結果になってるか心配)

--- gittest/Assets/Tools/Transiton/Scripts/TransitionManager.cs
+++ gittest/Assets/Tools/Transiton/Scripts/TransitionManager.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /// <summary>
 /// シーン管理　シングルトン
 /// </summary>
 public class TransitionManager : SingletonMonoBehaviour<TransitionManager>
 {
-
+    // マスクトランジション機能
     private MaskTransition m_MaskTransition;
+
+    //　読み込み率を表示するスライダー
+    [SerializeField] private Slider slider;
+
 
     //初期化メソッド(初アクセスまたはAwake時のどちらか一度だけ実行される)
     protected override void Init()
@@ -21,12 +26,20 @@ public class TransitionManager : SingletonMonoBehaviour<TransitionManager>
 
         // ---------- コンポーネント取得 ----------
         m_MaskTransition = GetComponent<MaskTransition>();
+
+        // ---------- スライダーを隠す ------------
+        slider.gameObject.SetActive(false);
     }
 
-
+    /// <summary>
+    /// シーン切り替え
+    /// </summary>
+    /// <param name="sceneName"></param>
+    /// <param name="time"></param>
     public void ChangeSceneTransiton(string sceneName,float time)
     {
-        StartCoroutine(LoadScene(sceneName, time));
+        if (!m_MaskTransition.Running)
+            StartCoroutine(LoadScene(sceneName, time));
     }
 
     IEnumerator LoadScene(string sceneName, float time)
@@ -34,16 +47,46 @@ public class TransitionManager : SingletonMonoBehaviour<TransitionManager>
         // ----- フェードイン -----
         m_MaskTransition.BeginTransition_In(time);
 
+        // ----- プログレスバー -----
+        slider.gameObject.SetActive(true);
+
         // ----- フェードイン終了まで待つ ----
         while (m_MaskTransition.Running)
         {
             yield return new WaitForEndOfFrame();
         }
 
+        // ----- シーンのロード非同期 -----
+        AsyncOperation async =  SceneManager.LoadSceneAsync(sceneName);
 
-        // ----- シーンのロード -----
-        SceneManager.LoadScene(sceneName);
+        // ----- シーン遷移をストップ ----
+        async.allowSceneActivation = false;
 
+        //　読み込みが終わるまで進捗状況をスライダーの値に反映させる
+        while (true)
+        {
+            yield return null;
+
+            Debug.Log(async.progress);
+            slider.value = async.progress;
+
+            if (async.progress >= 0.9f)
+            {
+                
+                slider.value = 1.0f;
+
+                // ----- シーン遷移を許可 ----
+                async.allowSceneActivation = true;
+
+                yield return new WaitForSeconds(1.0f);
+
+                break;
+            }
+
+        }
+
+        // ----- プログレスバー -----
+        slider.gameObject.SetActive(false);
 
         // ----- フェーアウト -----
         m_MaskTransition.BeginTransition_Out(time);
@@ -54,8 +97,9 @@ public class TransitionManager : SingletonMonoBehaviour<TransitionManager>
             yield return new WaitForEndOfFrame();
         }
 
-
         yield return null;
     }
+
+
 
 }
