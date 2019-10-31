5546d9274bdc0bdb60be2d7ba1c3ec76d6f5efbb
MasayaHayashi <nodoka11@hotmail.co.jp>
2019-11-01 02:48:08 +0900

    Add: Aura2アセット追加 Add: ウィルスクラス追加 現在スクリプタブルオブジェクトを使ったウィルス管理を作成中

---
+++ gittest/Assets/Viruses/Scripts/ViruseBase.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// ウィルス
+// Author : MasayaHayashi
+
+public class ViruseBase : MonoBehaviour
+{
+    [SerializeField, Header("ウィルス用オブジェクト")]
+    private ViruseData viruseObj;
+
+    private ViruseData nextViruseObj;               // 次の進化先
+
+    private const float EffectTime  = 2.0f;         // 演出時間
+    private bool        isEndEffect = false;        // 進化演出が終了したか
+
+    private void Awake()
+    {
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        changeSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            // 演出開始
+            StartCoroutine(StartEvolution(0));
+        }
+    }
+
+    // 進化開始
+    private IEnumerator StartEvolution(ViruseData.EvolutionType index)
+    {
+        Debug.Log("演出中");
+
+        yield return new WaitForSeconds(EffectTime);
+
+        // 次の進化先登録
+        nextViruseObj = viruseObj.nextEvolutions[(int)index];
+        viruseObj = nextViruseObj;
+
+        // 画像変更
+        changeSprite();
+    }
+
+    private void changeSprite()
+    {
+        gameObject.GetComponent<Image>().sprite = viruseObj.sprites[0];
+    }
+
+}
---
+++ gittest/Assets/Viruses/Scripts/ViruseData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ウィルス用データ
+// Author:MasayaHayashi
+
+[CreateAssetMenu(menuName = "MyScriptable/Create ViruseData")]
+public class ViruseData : ScriptableObject
+{
+    [SerializeField, Header("ウィルス名")]
+    public string name;
+
+    [SerializeField, Header("ウィルス画像")]
+    public List<Sprite> sprites;
+
+    [SerializeField, Header("次の進化先")]
+    public List<ViruseData> nextEvolutions;
+
+    public enum EvolutionType
+    {
+        GOOD = 0,
+        VERY_GOOD,
+        PARFECT,
+    }
+
+
+}
