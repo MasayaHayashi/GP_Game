e749deedcd58e3389ba9ccb67353d300af892bb7
Ryo <r1995ngspring@gmail.com>
2019-11-02 01:03:57 +0900

    fix:上手くプッシュできるかテスト、ライティング軽量設定に変更

---
+++ gittest/Assets/Prottype_Effect/Scripts/DialogEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// ウイルス解析ダイアログ演出
+/// </summary>
+public class DialogEffect : MonoBehaviour
+{
+  
+    private Image m_Background;
+
+    [SerializeField] private Vector2 dialogSize;
+   
+
+    private void Awake()
+    {
+        m_Background = GetComponent<Image>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
