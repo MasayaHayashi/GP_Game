f38f0616d8d1091c26685b50576ee17dcd38d404
MasayaHayashi <kakashino1desuna@gmail.com>
2019-11-08 12:06:43 +0900

    add: ワイヤーフレーム、球体演出追加

---
+++ gittest/Assets/CyberEffects/Scripts/Rotatar.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 回転用スクリプト
+// Author : HayashiMasaya
+
+public class Rotatar : MonoBehaviour
+{
+    [SerializeField, Header("回転軸")]
+    private Vector3 axis;
+
+    [SerializeField, Header("回転スピード")]
+    private float rotSpeed;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float angle = rotSpeed * Time.deltaTime;
+        Quaternion qua = Quaternion.AngleAxis(angle, axis);
+
+        transform.rotation = qua * transform.rotation;
+    }
+}
