dca584f5421c4f0330272ac1a5876885a620af09
Ryo <r1995ngspring@gmail.com>
2019-11-08 17:44:27 +0900

    Add:カメラ移動と年代表示

---
+++ gittest/Assets/Prottype_Effect/Scripts/UI_AGE.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// 年代表示
+/// 加藤　遼
+/// </summary>
+public class UI_AGE : MonoBehaviour
+{
+
+    [SerializeField] private Text m_AgeText;
+    [SerializeField] private int m_CurrentAge = 2019;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_CurrentAge++;
+
+
+        // テキスト更新
+        m_AgeText.text = m_CurrentAge.ToString();
+
+    }
+}
