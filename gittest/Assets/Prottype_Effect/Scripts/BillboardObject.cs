cd2829f89c820060d4a74426af08f430609fba71
Ryo <r1995ngspring@gmail.com>
2019-11-15 14:16:06 +0900

    Add:薬の効果演出パーティクル追加

---
+++ gittest/Assets/Prottype_Effect/Scripts/BillboardObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y , transform.localScale.z * -1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p = Camera.main.transform.position;
+         p.x = transform.position.x;
+       
+        transform.LookAt(p);
+
+      
+
+        // Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z,transform.rotation.w);
+
+        // transform.rotation = rot ;
+    }
+}
