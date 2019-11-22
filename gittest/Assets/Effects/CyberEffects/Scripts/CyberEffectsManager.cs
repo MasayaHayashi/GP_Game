573b1b0d06c5246ae68edd0f1cb155d4969cb18f
MasayaHayashi <kakashino1desuna@gmail.com>
2019-11-22 11:47:41 +0900

    update: サイバーエフェクトの色変更作成中 add: コントローラー用設定対応

---
+++ gittest/Assets/Effects/CyberEffects/Scripts/CyberEffectsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author : MasayaHayashi
+// 周りのサイバーエフェクト管理
+
+public class CyberEffectsManager : MonoBehaviour
+{
+    [SerializeField, Header("エフェクト")]
+    private List<GameObject> cyberEffects;
+
+    private List<TrailRenderer> trails;
+    private Color startColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(cyberEffects.Count <= 0)
+        {
+            Debug.LogError("cyberEffects Material is Not Set");
+        }
+
+        foreach(GameObject cyberEffect in cyberEffects)
+        {
+            trails.Add(cyberEffect.GetComponent<TrailRenderer>());
+        }
+
+        startColor = cyberEffects[0].GetComponent<TrailRenderer>().startColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changeColor()
+    {
+
+    }
+
+   
+}
