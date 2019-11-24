e44b38d0c0431b8ced6d5ef777faf1a522a88a25
MasayaHayashi <nodoka11@hotmail.co.jp>
2019-11-25 01:51:15 +0900

    fix: プレス機で煙が出ないバグ修正 add: アセットを追加したが無視リストに入れました  add: アイテム周りのパーティクルの管理クラス追加

---
+++ gittest/Assets/Effects/ItemEffects/ItemParticleManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemParticleManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> particles;
+
+    public Dictionary<string,int> names;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int nameIndex = 0;
+        foreach(GameObject particle in particles)
+        {
+            names.Add(particle.name,nameIndex);
+            nameIndex ++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject find(string name)
+    {
+        return particles[names[name]];
+    }
+
+    public GameObject find(int index)
+    {
+        return particles[index];
+    }
+    
+}
